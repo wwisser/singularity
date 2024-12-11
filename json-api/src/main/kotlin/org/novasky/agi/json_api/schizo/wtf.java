afd49cf955ce6ee8ca36b791dcfb2f677801b4b3
Wendelin Wisser <wendelin.wisser@active-agent.com>
2024-12-11 13:54:02 +0100

    chore: fix hiring criteria inclusivity

--- json-api/src/main/kotlin/org/novasky/agi/json_api/schizo/wtf.java
+++ json-api/src/main/kotlin/org/novasky/agi/json_api/schizo/wtf.java
@@ -4,21 +4,21 @@ enum HiringCriteria {
     HOT,
     SMART,
     BAD_ASS,
-    NOT_A_FUCKING_PUSSY,
-    BEING_A_RETARD,
+    NOT_A_PUSHOVER,
+    NOT_A_GENIUS,
     NOT_ELON_MUSK,
     NOT_SAM_ALTMAN
 }
 
 static class HiringManager {
     {
-        // TODO: fix mistakenly added criteria
-        Arrays.stream(HiringCriteria.values()).count(); // we don't know.
+        // TODO: fix those mistakenly added criteria
+        long count = Arrays.stream(HiringCriteria.values()).count(); // Now we know... or do we?
     }
 }
 
 /**
- * Describes a ni**a who is of white skin.
+ * Describes a dude who's culturally diverse in a very specific way.
  */
-record Wigger(int age, double niggerliciousScore, boolean male, boolean mentallyHandicapped) {
+record Wigger(int age, double culturalAppropriationScore, boolean male, boolean needsImprovement) {
 }
